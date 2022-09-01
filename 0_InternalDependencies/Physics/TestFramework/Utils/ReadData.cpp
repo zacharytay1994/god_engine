eb83585e697712d8fbf6f78c0e022e52bd9888da
Darrick Ching <darrick.c@digipen.edu>
2022-09-01 09:38:35 +0800

    added jolt physics lib

---
+++ 0_InternalDependencies/Physics/TestFramework/Utils/ReadData.cpp
@@ -0,0 +1,24 @@
+// SPDX-FileCopyrightText: 2021 Jorrit Rouwe
+// SPDX-License-Identifier: MIT
+
+#include <TestFramework.h>
+#include <fstream>
+#include <Utils/ReadData.h>
+#include <Utils/Log.h>
+
+// Read file contents
+Array<uint8> ReadData(const char *inFileName)
+{
+	Array<uint8> data;
+	ifstream input(inFileName, std::ios::binary);
+	if (!input)
+		FatalError("Unable to open file: %s", inFileName);
+	input.seekg(0, ios_base::end);
+	ifstream::pos_type length = input.tellg();
+	input.seekg(0, ios_base::beg);
+	data.resize(size_t(length));
+	input.read((char *)&data[0], length);
+	if (!input)
+		FatalError("Unable to read file: %s", inFileName);
+	return data;
+}
