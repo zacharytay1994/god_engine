8995154c9ef78f8cbc46e6f6194563a6036c2e90
Darrick Ching <darrick.c@digipen.edu>
2022-11-17 12:45:26 +0800

    dunno...

--- 1_Game/Assets/GameAssets/Scripts/WinLose.lua
+++ 1_Game/Assets/GameAssets/Scripts/WinLose.lua
@@ -2,7 +2,7 @@
 function C_WinLose()
     local var = {
     timer = 0.0,
-    lifetime = 2.0,
+    lifetime = 2.0
     }
     return function()
         return var
@@ -11,11 +11,14 @@ end
 
 --[IsSystem]
 function S_WinLose(e)
-
+    print("Hi winlose")
     local winLose = GetComponent(e, "C_WinLose")
-    local player = GetComponent(e, "C_Character")
+    print("Hi")
+    local player = GetComponent(GetEntity("Player"), "C_Character")
+    print("Hi player")
     local gui_object = GetGUIObject(e)
-
+    print("Hi UI")
+    
     if(player.isDead == true)
     then
         print("Player is dead")
