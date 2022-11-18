45e4b8a71a197b9fd1bb7c65462c7de68342a792
Jia Min <jeejiamin@gmail.com>
2022-11-19 01:10:52 +0800

    added a few objects in scene

--- 1_Game/Assets/GameAssets/Scripts/SplashScreen.lua
+++ 1_Game/Assets/GameAssets/Scripts/SplashScreen.lua
@@ -8,18 +8,21 @@ function C_SplashScreen()
     end
 end
 
--- number of corals to spawn
-local corals_row_min_ = 0
-local corals_row_max_ = 20
-local corals_column_min_ = -2
-local corals_column_max_ = 8
+-- object location in scene
+local objs_row_min_ = 0
+local objs_row_max_ = 20
+local objs_column_min_ = -2
+local objs_column_max_ = 8
+local objs_row_max_div2_ = objs_row_max_ / 2
 
 -- random generated rocks
 function GenerateRandomRocks(x, y, z)
 
-    local rng_pos = GenerateRandomNumberInRange(1, 10) / 100.0
+    -- generate random position data
+    local rng_pos = GenerateRandomNumberInRange(1, 5)
+
     -- create rock
-    local rock_entity = InstancePrefabNow("SS_Rock", rng_pos + x, rng_pos + y, z)
+    local rock_entity = InstancePrefabNow("SS_Rock", rng_pos + x, y, rng_pos + z)
 
     -- random generated seeds
     ChangeModel(rock_entity, "rock0" .. tostring(GenerateRandomNumberInRange(1, 4)))
@@ -32,16 +35,28 @@ function GenerateRandomRocks(x, y, z)
     rock_transform.scale.x = rng_scale
     rock_transform.scale.y = rng_scale
     rock_transform.scale.z = rng_scale
-    rock_transform.position.y = rock_transform.scale.y;
+    rock_transform.position.y = rock_transform.position.y + (rock_transform.scale.y / 2.0);
 end
 
+-- random generated seaweeds
 function GenerateRandomSeaweeds(x, y, z)
 
-    local seaweed_entity = InstancePrefabNow("SS_Seaweeds", x, y, z)
+    -- generate random position
+    local rng_pos = GenerateRandomNumberInRange(1, 10)
 
-    ChangeModel(seaweed_entity, "Plant_Seaweed0" .. tostring(GenerateRandomNumberInRange(1, 3)))
+    if rng_pos + x ~= objs_row_max_div2_ then
+        local seaweed_entity = InstancePrefabNow("SS_Seaweed", rng_pos + x, y, rng_pos + z)
 
-    local seaweed_transform = GetTransform(seaweed_entity)
+        ChangeModel(seaweed_entity, "Plant_Seaweed0" .. tostring(GenerateRandomNumberInRange(1, 3)))
+
+        local seaweed_transform = GetTransform(seaweed_entity)
+
+        local rng_scale = GenerateRandomNumberInRange(1, 10) / 10.0
+        seaweed_transform.scale.x = rng_scale
+        seaweed_transform.scale.y = rng_scale
+        seaweed_transform.scale.z = rng_scale
+        seaweed_transform.position.y = 1.0 * seaweed_transform.scale.y
+    end
 
 end
 
@@ -49,28 +64,31 @@ end
 function OnLoad_SplashScreen()
 
     local camera      = FindCameraObject()
-    camera.position.x = 0
+    camera.position.x = objs_row_max_div2_
     camera.position.y = 2
     camera.position.z = 10
 
     -- create corals based on row and column
-    for j = corals_column_min_, corals_column_max_, 1 do
-        for i = corals_row_min_, corals_row_max_, 1 do
-            GenerateRandomRocks(i - (corals_row_max_ / 2), 0, j + 0.5)
+    for j = objs_column_min_, objs_column_max_, 1 do
+        for i = objs_row_min_, objs_row_max_, 1 do
+            GenerateRandomRocks(i, 0, j)
+            GenerateRandomSeaweeds(i, 0, j)
         end
     end
 
     -- create directional light at the door
-    InstancePrefab("SS_DirectionalLight", 0, 6, -6)
+    InstancePrefab("SS_DirectionalLight", objs_row_max_div2_, 6, -6)
+
+    InstancePrefab("SS_RockFlat", objs_row_max_div2_, 0, -7)
 
     -- create door at the end of the corals
-    InstancePrefab("SS_Door", 0, 0, -corals_column_max_)
+    InstancePrefab("SS_Door", objs_row_max_div2_, 2.5, -7)
 
     -- create terrain floor
-    InstancePrefab("SS_Terrain", 0, 0, -38)
+    InstancePrefab("SS_Terrain", objs_row_max_div2_, 0, -38)
 
     -- create rectagular plain floor / using precreated prefab
-    InstancePrefab("FloorPrefab", 0, 0, 0)
+    InstancePrefab("FloorPrefab", objs_row_max_div2_, 0, 0)
 
     print("[SplashScreen.lua] OnLoad_SplashScreen")
 end
@@ -100,7 +118,7 @@ function S_SplashScreen(e)
     door_transform.position.y = door_transform.scale.y
 
     -- scale the door
-    if door_transform.scale.x < 4.5 and not IsMoving then
+    if door_transform.scale.x < 5 and not IsMoving then
         door_transform.scale.x = door_transform.scale.x + speed
         door_transform.scale.y = door_transform.scale.y + speed
         door_transform.scale.z = door_transform.scale.z + speed
