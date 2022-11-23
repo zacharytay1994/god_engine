b8135b7cf6d02e09e7488dd7f6761f0ed5edddc6
Jagureo <jagnzet@gmail.com>
2022-11-23 13:50:03 +0800

    Fixed dice preview script

--- 1_Game/Assets/GameAssets/Scripts/DicePreview.lua
+++ 1_Game/Assets/GameAssets/Scripts/DicePreview.lua
@@ -1,12 +1,18 @@
 --[IsComponent]
 function C_DicePreview()
-local var = {
-}
-return function()
-return var
-end
+	local var = {
+		--[SerializeString]
+		fodder_text = "DicePreview is attached!",
+	}
+	return function()
+		return var
+	end
 end
 
 --[IsSystem]
 function S_DicePreview(e)
+	local transform = GetTransform(e)
+	transform.rotation.x = transform.rotation.x + 1
+	transform.rotation.y = transform.rotation.y + 1
+	transform.rotation.z = transform.rotation.z + 1
 end
