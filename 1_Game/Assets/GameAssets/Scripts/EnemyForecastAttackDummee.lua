ed1173026c0677261f68ff26077934cda398ff9e
minye88 <l.minye@digipen.edu>
2022-11-23 13:58:27 +0800

    AttackIndicator now uses DrawLine() instead of budget draw line

--- 1_Game/Assets/GameAssets/Scripts/EnemyForecastAttackDummee.lua
+++ 1_Game/Assets/GameAssets/Scripts/EnemyForecastAttackDummee.lua
@@ -4,10 +4,16 @@
 function C_EnemyForecastAttackDummee()
     local var = {
 
+        -- once movement forecast is done, EnemyController will tell this script to 
+        -- perform an attack forecast by setting this boolean to true
         performAttackForecast = false,
 
-        -- Vector3 
-        attackDirection = { }
+       -- Vector3 which stores the direction of the Squinky's attack
+        attackDirection = { },
+
+        -- will draw a line (if player is in attack range), but only if
+        -- EnemyForecast did not pause drawing of lines.
+        drawLine = false
         
     }
     return function()
@@ -21,23 +27,37 @@ function S_EnemyForecastAttackDummee(e)
     local forecastComponent = GetComponent(e, "C_EnemyForecastAttackDummee")
     local enemyController = GetComponent(e, "C_EnemyController")
 
+    -- if told to perform attack forecast by EnemyController, then set drawLine to true
     if (forecastComponent.performAttackForecast == true) then
             
         -- if forecastIndicator is in the same lane as the Player, then forecast attack
         if (EnemyForecastAttackDummeeSameLane(e, enemyController.indicatorEntity, GetEntity("Player")) == true) then
             print("[EnemyForecastAttackDummee.lua] Indicator in the same lane as Player, proceeding to forecast attack.")
 
-            -- SHOW ATTACK FORECAST GRAPHIC HERE
-            EnemyForecastAttackDummeeDrawArrow(e, enemyController.indicatorEntity, GetEntity("Player"))
-
+            forecastComponent.drawLine = true
             enemyController.attackForecast = false
             forecastComponent.performAttackForecast = false
         
         -- else, enemy cannot attack player in the following turn, don't need to forecast anything
         else
             print("[EnemyForecastAttackDummee.lua] Indicator NOT in the same lane as Player, nothing to forecast attack.")
+            forecastComponent.drawLine = false
             enemyController.attackForecast = false
             forecastComponent.performAttackForecast = false
+        end
+    end
+
+    -- if can drawLine and EnemyForecast has not paused the drawing of lines, then draw the line between the movement indicator and Player.    
+    if (forecastComponent.drawLine and GetComponent(GetEntity("EnemyForecast"), "C_EnemyForecast").pauseDrawLine == false) then
+        
+        -- safety check
+        if (enemyController.indicatorEntity ~= nil) then
+            
+            -- getting the positions required for drawing lines 
+            local forecastIndicatorPosition = GetTransform(enemyController.indicatorEntity).position
+            local playerPosition = GetTransform(GetEntity("Player")).position
+            
+            DrawLine(forecastIndicatorPosition.x, forecastIndicatorPosition.y, forecastIndicatorPosition.z, playerPosition.x, playerPosition.y, playerPosition.z, 1, 0, 0, 0, 10)
         end
     end
 end
@@ -93,39 +113,4 @@ function EnemyForecastAttackDummeeSameLane(e, forecastIndicator, player)
     end
 
     return result
-end
-
--- draw a line connecting the forecast indicator and the player
-function EnemyForecastAttackDummeeDrawArrow(e, forecastIndicator, player)
-
-    -- calculate attackIndicator's position and scale
-    local forecastIndicatorPosition = GetTransform(forecastIndicator).position
-    local playerPosition = GetTransform(player).position
-    local attackIndicatorPosition = { }
-    attackIndicatorPosition[1] = (forecastIndicatorPosition.x + playerPosition.x) / 2
-    attackIndicatorPosition[2] = (forecastIndicatorPosition.y + playerPosition.y) / 2
-    attackIndicatorPosition[3] = (forecastIndicatorPosition.z + playerPosition.z) / 2
-    -- print("[EnemyForecastAttackDummee.lua] Check attackIndicatorPosition:", attackIndicatorPosition[1], attackIndicatorPosition[2], attackIndicatorPosition[3])
-    
-    -- create an instance of an attackIndicator
-    local attackIndicator = InstancePrefabNow("AttackIndicator", attackIndicatorPosition[1], attackIndicatorPosition[2], attackIndicatorPosition[3])
-
-    -- add the instance to EnemyForecast's indicatorsList
-    local enemyForecastEntity = GetEntity("EnemyForecast")
-    if (enemyForecastEntity ~= -1) then
-        local enemyForecastComponent = GetComponent(enemyForecastEntity, "C_EnemyForecast")
-        enemyForecastComponent.indicatorsList[#enemyForecastComponent.indicatorsList + 1] = attackIndicator
-    else
-        print("[EnemyForecastAttackDummee.lua] ERROR: EnemyForecast entity cannot be found!")
-    end
-
-    -- scale the attackIndicator (a scale.x or scale.z of 1.0 is exactly the length of one tile)
-    if (forecastIndicatorPosition.x - playerPosition.x ~= 0) then
-        -- if forecastIndicator and player are on different x-axis then scale according to x-axis distance between each other
-        GetTransform(attackIndicator).scale.x = (forecastIndicatorPosition.x - playerPosition.x) / 2
-    else
-        -- if forecastIndicator and player are on different z-axis then scale according to z-axis distance between each other
-        GetTransform(attackIndicator).scale.z = (forecastIndicatorPosition.z - playerPosition.z) / 2
-    end
-
 end
