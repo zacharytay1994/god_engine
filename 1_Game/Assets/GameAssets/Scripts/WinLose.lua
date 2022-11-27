a7b827484db93c1ebf0e5af1005f8c64df38559d
Ting2 <tingwoonlovecats@gmail.com>
2022-11-27 17:15:04 +0800

    Ambience SFX update

--- 1_Game/Assets/GameAssets/Scripts/WinLose.lua
+++ 1_Game/Assets/GameAssets/Scripts/WinLose.lua
@@ -9,15 +9,22 @@ function C_WinLose()
     end
 end
 
+local sfxTriggered = false
+
 --[IsSystem]
 function S_WinLose(e)
-   
     local winLose = GetComponent(e, "C_WinLose")
 
     local gui_object = GetGUIObject(e)
     
     if(isAlive == false)
     then
+		if sfxTriggered == false then
+			-- trigger sound effect
+			InstancePrefab("SFX_PlayerDeath",0,0,0)
+			sfxTriggered = true
+		end
+
         --print("Player is dead")
         gui_object.active = true
     else
