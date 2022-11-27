f8ead8b689fa9edbde8ca188167646f514aa7502
minye88 <l.minye@digipen.edu>
2022-11-27 14:21:04 +0800

    Enemies now spawn at random locations at the start of each level

--- 1_Game/Assets/GameAssets/Scripts/SpawnEnemy.lua
+++ 1_Game/Assets/GameAssets/Scripts/SpawnEnemy.lua
@@ -1,5 +1,9 @@
--- Attached to GameManager entity.
+-- Attached to SpawnEnemy entity.
 -- At the start of each level, this script will spawn enemies at random locations.
+-- After spawning, SpawnEnemy entity will destroy itself.
+
+-- TODO: 
+-- 1) Create a list of enemies and automate the spawn instead of having different loops for each type of enemy
 
 --[IsComponent]
 function C_SpawnEnemy()
@@ -21,18 +25,33 @@ end
 function S_SpawnEnemy(e)
 
     local spawnEnemyComponent = GetComponent(e, "C_SpawnEnemy")
-    local tileList = EntitiesWithScriptComponent("C_FloorTile")
+    local unoccupiedTileList = AllUnoccupiedFloorTiles()
+    print("[SpawnEnemy.lua] Spawning enemies!")
 
-    while (spawnEnemyComponent.numberOfDummee > 0) do
+    -- spawn Dummees
+    for i = 1, spawnEnemyComponent.numberOfDummee do
     
-        -- spawn dummees here
-    
+        -- first, get a random tile
+        local randomTile = RandomUnoccupiedFloorTile()
+        local randomTileGridCell = GetGridCell(randomTile)
+
+        -- then instance a dummee on that tile
+        InstancePrefabParentedOnGridNow(GetEntity("Floor"), "Dummee", randomTileGridCell.x, randomTileGridCell.y + 1, randomTileGridCell.z)    
     end
 
-    while (spawnEnemyComponent.numberOfSquinky > 0) do
+    -- then do the same for Squinkies
+    for i = 1, spawnEnemyComponent.numberOfSquinky do
     
-        -- spawn squinkies here
-    
+        -- first, get a random tile
+        local randomTile = RandomUnoccupiedFloorTile()
+        local randomTileGridCell = GetGridCell(randomTile)
+
+        -- then instance a dummee on that tile
+        InstancePrefabParentedOnGridNow(GetEntity("Floor"), "Squinky", randomTileGridCell.x, randomTileGridCell.y + 1, randomTileGridCell.z)
     end
 
+    print("[SpawnEnemy.lua] Removing SpawnEnemy entity!")
+
+    -- remove itself after spawning enemies
+    RemoveInstance(e)
 end
