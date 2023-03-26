9fb95848de9b5f6beed7818ba2f90e1708544e99
Zachary <zacharytay1994@gmail.com>
2023-03-26 21:59:10 +0800

    bubbles on tiles

--- 1_Game/Assets/GameAssets/Scripts/Gam350/350TileDrop.lua
+++ 1_Game/Assets/GameAssets/Scripts/Gam350/350TileDrop.lua
@@ -2,7 +2,8 @@
 function C_350TileDrop()
     local var = {
         --[SerializeFloat]
-        DropSpeed = 5.0
+        DropSpeed = 5.0,
+        EmissionRate = 0.06
     }
     return function()
         return var
@@ -16,4 +17,25 @@ function S_350TileDrop(e)
     local transform = GetTransform(e)
     local dt = GetDeltaTime()
     transform.position = LerpVec3(transform.position.x, transform.position.y, transform.position.z, 0, 0, 0, dt*C_350TileDrop.DropSpeed)
+    if (transform.position.y > 0.1) then
+        if (C_350TileDrop.EmissionRate > 0) then
+            C_350TileDrop.EmissionRate = C_350TileDrop.EmissionRate - dt;
+        else
+            C_350TileDrop.EmissionRate = 0.04
+            local bubble = InstancePrefabParentedNow(e, "BubbleParticle3D", 0, 0, 0)
+            local bubble_particle = GetComponent(bubble, 'C_BubbleParticle')
+            local bubble_transform = GetTransform(bubble)
+            local rand_size = GenerateRandomNumberInRange(2,8)/10.0
+            local rand_x = GenerateRandomNumberInRange(1,10)/10.0-0.5
+            local rand_z = GenerateRandomNumberInRange(1,10)/10.0-0.5
+            bubble_particle.initialHeight = transform.position.y
+            bubble_transform.scale.x = rand_size
+            bubble_transform.scale.y = rand_size
+            bubble_transform.scale.z = rand_size
+            bubble_transform.position.x = rand_x
+            bubble_transform.position.z = rand_z
+            bubble_particle.Speed = rand_size * 20.0
+            bubble_particle.Lifetime = 6.0
+        end
+    end
 end
