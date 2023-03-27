4f4a6cb5b2f224612c77da82d6651bc1e7faa8d4
dragonsister199 <93319554+nicoletlw@users.noreply.github.com>
2023-03-27 11:41:01 +0800

    sotong change, left one door, yellow bubbles stop halfway
    - might need to change 2d outlines
    - change side stuff to have colour
    - remove the big branch
    - fix terrain and caustics

--- 1_Game/Assets/GameAssets/Scripts/SplashScreen.lua
+++ 1_Game/Assets/GameAssets/Scripts/SplashScreen.lua
@@ -148,11 +148,13 @@ function GenerateSeahorse(vel_x, vel_y, vel_z, pos_x, pos_y, pos_z, index)
         local seahorse_transform = GetTransform(seahorse)
         if vel_x < 0 then
             seahorse_transform.rotation.y = -80
+        else
+            seahorse_transform.rotation.y = 80
         end
-        local rng_scale = GenerateRandomNumberInRange(5, 10) / 10.0
-        seahorse_transform.scale.x = rng_scale
-        seahorse_transform.scale.y = rng_scale
-        seahorse_transform.scale.z = rng_scale
+        --local rng_scale = GenerateRandomNumberInRange(5, 10) / 10.0
+        --seahorse_transform.scale.x = rng_scale
+        --seahorse_transform.scale.y = rng_scale
+        --seahorse_transform.scale.z = rng_scale
     end
 end
 
